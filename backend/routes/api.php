c5677ae81c7a446472f506346fb8fbc64bb726d1
bpkopania <b.p.kopania@gmail.com>
2023-11-19 15:45:23 +0100

    userLogining

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\ApiAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+    Route::post('/logout', [ApiAuthController::class, 'logout'])->name('logout.api');
+});
+
+Route::middleware('auth:api')->group(function () {
+    // our routes to be protected will go in here
+});
+
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+    Route::post('/login', [ApiAuthController::class, 'login'])->name('login.api');
+    Route::post('/register',[ApiAuthController::class, 'register'])->name('register.api');
 });
